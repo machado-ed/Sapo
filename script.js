cf0c55bd554eed8436fbe87db82250b9577e8038
Edicley Machado <edicleya7@gmail.com>
2026-06-16 22:22:53 +0000

    ssss

---
+++ script.js
@@ -0,0 +1,14 @@
+let bgPosX = 0
+let fgPosX = 0
+let rDis = parseFloat(42195)
+
+function move()
+{
+    bgPosX+=2;
+    fgPosX += 16
+    rDis -= 0.1
+    document.getElementById("stepL").innerText = 'faltando:'+ rDis.toFixed(1)
+    document.documentElement.style.setProperty('--bgPosX',bgPosX+'px');
+    document.documentElement.style.setProperty('--fgPosX',fgPosX+'px');
+    
+}
