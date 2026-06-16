d2bc8955356dc5f2ab1e9b60e510cf6eda6cf295
machado-ed <edicleya7@gmail.com>
2026-06-16 20:27:02 -0300

    feito acumulador de pontos

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let bgPosX = 0
 let fgPosX = 0
 let rDis = parseFloat(42195)
+let coins = 0
 
-function move()
-{
-    bgPosX+=2;
+function move() {
+    bgPosX += 2;
     fgPosX += 16
     rDis -= 0.1
-    document.getElementById("stepL").innerText = 'faltando:'+ rDis.toFixed(1)
-    document.documentElement.style.setProperty('--bgPosX',bgPosX+'px');
-    document.documentElement.style.setProperty('--fgPosX',fgPosX+'px');
-    
+    document.getElementById("stepL").innerText = 'faltando:' + rDis.toFixed(1)
+    document.documentElement.style.setProperty('--bgPosX', bgPosX + 'px');
+    document.documentElement.style.setProperty('--fgPosX', fgPosX + 'px');
+    coins++;
+    coinsDisplay.textContent = coins;
 }
+
+const coinsDisplay = document.getElementById("coins");
