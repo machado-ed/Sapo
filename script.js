e5b77234fb1d175cb90ca8409499f589dae2abd0
Gabriel0V0 <gabriel.hsilva@unifebe.edu.br>
2026-06-17 17:59:40 -0300

    codigo pulando

--- script.js
+++ script.js
@@ -7,11 +7,18 @@ function move() {
     bgPosX += 2;
     fgPosX += 16
     rDis -= 0.1
+    clearTimeout()
+    document.documentElement.style.setProperty('--jmpHg','0px');
     document.getElementById("stepL").innerText = 'faltando:' + rDis.toFixed(1)
     document.documentElement.style.setProperty('--bgPosX', bgPosX + 'px');
     document.documentElement.style.setProperty('--fgPosX', fgPosX + 'px');
+    document.documentElement.style.setProperty('--jmpHg','10px');
     coins++;
     coinsDisplay.textContent = coins;
+    
+    setTimeout(() => {
+       document.documentElement.style.setProperty('--jmpHg','0px'); 
+    }, 200);
 }
 
 
