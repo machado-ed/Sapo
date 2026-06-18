0bb028a8a53f7c858fa36db91d302c9596f4ca95
Gabriel0V0 <gabriel.hsilva@unifebe.edu.br>
2026-06-18 14:30:09 -0300

    barra de progressao

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 let bgPosX = 0
 let fgPosX = 0
-let rDis = parseFloat("4219")
+let rDis = parseFloat("42195")
 let coins = 0
-
+let pDis = 0
+let wDis = 0
 function move() {
     bgPosX += 2;
     fgPosX += 16
-    rDis -= 0.1
+    rDis -= 200
+    wDis += 200
+    pDis = wDis / 42195 
     clearTimeout()
     document.documentElement.style.setProperty('--jmpHg','0px');
     document.getElementById("stepL").innerText = 'faltando:' + rDis.toFixed(1)
     document.documentElement.style.setProperty('--bgPosX', bgPosX + 'px');
     document.documentElement.style.setProperty('--fgPosX', fgPosX + 'px');
     document.documentElement.style.setProperty('--jmpHg','10px');
+    document.documentElement.style.setProperty('--percentage', pDis);
+    document.getElementById("progressBar").value = pDis
     coins++;
     coinsDisplay.textContent = coins;
     
