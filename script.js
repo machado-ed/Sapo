37f44fc188cfce469c6ae7df5870ee0a9d727568
Gabriel0V0 <gabriel.hsilva@unifebe.edu.br>
2026-06-19 17:39:18 -0300

    mult

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
+//Variveis pro css
 let bgPosX = 0
 let fgPosX = 0
 let rDis = parseFloat("42195")
-let coins = 0
 let pDis = 0
 let wDis = 0
 const cssVar = document.documentElement.style
 const coinsDisplay = document.getElementById("coins");
+//variaveis pro jogo
+let coins = 0
 let coinVal = 1
 let walkVal = 0.1
+let coinMult = 1
+let walkMult = 1
 function move() {
-    bgPosX += 20 * walkVal;
-    fgPosX += 160 * walkVal;
+    // move o fundo e o chao de acordo como o pulo
+    bgPosX += 20 * (walkVal * walkMult);
+    fgPosX += 160 * (walkVal * walkMult);
+    //atualiza o marcador de distancia
     rDis -= walkVal
     wDis += walkVal
-    pDis = wDis / 42195 
+    pDis = wDis / 42195
     clearTimeout(coinJmp)
     cssVar.setProperty('--jmpHg','0px');
     document.getElementById("stepL").innerText = 'faltando:' + rDis.toFixed(1)
@@ -22,8 +28,8 @@ function move() {
     cssVar.setProperty('--jmpHg','10px');
     cssVar.setProperty('--percentage', pDis);
     document.getElementById("progressBar").value = pDis
-    coins+= coinVal;
-    coinsDisplay.textContent = coins;
+    coins+= coinVal * coinMult;
+    coinsDisplay.textContent = parseInt(coins);
     var coinJmp =setTimeout(() => {
        cssVar.setProperty('--jmpHg','0px'); 
     }, 100);
@@ -44,8 +50,18 @@ function valCoinUp()
             //falta incrementar o valor
             coinVal +=1;
             coins -= 10
-            coinsDisplay.textContent = coins;
+            coinsDisplay.textContent =parseInt(coins);
         }
         return null
 }
-function valCoinMult(){}
+function valCoinMult()
+{
+     if (coins >= 30)
+        {
+            //falta incrementar o valor
+            coinMult +=0.25;
+            coins -= 30
+            coinsDisplay.textContent = parseInt(coins);
+        }
+        return null
+}
