b79bb302e194bb645691a90b7671865575a067fa
Gabriel0V0 <gabriel.hsilva@unifebe.edu.br>
2026-06-19 16:57:07 -0300

    mudando por leitura

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ let rDis = parseFloat("42195")
 let coins = 0
 let pDis = 0
 let wDis = 0
-
+const cssVar = document.documentElement.style
+const coinsDisplay = document.getElementById("coins");
 let coinVal = 1
 let walkVal = 0.1
 function move() {
@@ -14,28 +15,27 @@ function move() {
     wDis += walkVal
     pDis = wDis / 42195 
     clearTimeout(coinJmp)
-    document.documentElement.style.setProperty('--jmpHg','0px');
+    cssVar.setProperty('--jmpHg','0px');
     document.getElementById("stepL").innerText = 'faltando:' + rDis.toFixed(1)
-    document.documentElement.style.setProperty('--bgPosX', bgPosX + 'px');
-    document.documentElement.style.setProperty('--fgPosX', fgPosX + 'px');
-    document.documentElement.style.setProperty('--jmpHg','10px');
-    document.documentElement.style.setProperty('--percentage', pDis);
+    cssVar.setProperty('--bgPosX', bgPosX + 'px');
+    cssVar.setProperty('--fgPosX', fgPosX + 'px');
+    cssVar.setProperty('--jmpHg','10px');
+    cssVar.setProperty('--percentage', pDis);
     document.getElementById("progressBar").value = pDis
     coins+= coinVal;
     coinsDisplay.textContent = coins;
     var coinJmp =setTimeout(() => {
-       document.documentElement.style.setProperty('--jmpHg','0px'); 
+       cssVar.setProperty('--jmpHg','0px'); 
     }, 100);
 }
 
 
-const coinsDisplay = document.getElementById("coins");
 //estou fazendo a divisao dos upgrades em melhora a obtencao de moedas 
 // e outra que melhora a progressao do objetivo
 //assim como multiplicadores respectivamente
 //nao sei direito como fazer para animar o autoclicker
 function jmpDis(){}
-function hpmHgt(){}
+function jpmHgt(){}
 function autoJmp(){}
 function valCoinUp()
 {
@@ -48,3 +48,4 @@ function valCoinUp()
         }
         return null
 }
+function valCoinMult(){}
